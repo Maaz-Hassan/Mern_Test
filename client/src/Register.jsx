21238621ac264b5a479e5c3e2910b46fb52eecb5
Maaz-Hassan <sengineer804@gmail.com>
2024-03-01 20:27:03 +0500

    Upload Code Frontend and backend

---
+++ client/src/Register.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Link, useNavigate } from 'react-router-dom'
+
+function Register() {
+    const [register , setRegister] = useState({})
+    const navigate = useNavigate();
+
+const handleChange = (e) => {
+  setRegister({
+    ...register,
+    [e.target.name]: e.target.value
+  })
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  axios.post('http://localhost:3001/register', register)
+  .then(result => {
+    if(result.data == "User already Registered"){
+      alert(result.data)
+    }
+    else{
+      alert(result.data)
+      navigate("/Login")
+    }
+})
+  .catch(err => console.log(err))
+}
+  return (
+    <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
+      <div className='bg-white p-3 rounded w-25'> 
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="name">
+              <strong>Name</strong>
+            </label>
+          <input 
+          type="text" 
+          placeholder='Enter Name'
+          autoComplete='off'
+          name='name'
+          className='form-control rounded-0'
+          onChange={handleChange}
+          />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+          <input 
+          type="email" 
+          placeholder='Enter Email'
+          autoComplete='off'
+          name='email'
+          className='form-control rounded-0'
+          onChange={handleChange}
+          />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+          <input 
+          type="password" 
+          placeholder='Enter Password'
+          autoComplete='off'
+          name='password'
+          className='form-control rounded-0'
+          onChange={handleChange}
+          />
+          </div>
+          <button type='submit' className='btn btn-success w-100 rounded-0'>Register</button>
+          <p>Already have an account?</p>
+          <Link to={"/Login"} className='btn btn-default border w-100 bg-light rounded-0 text-decoration none'>Login</Link>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Register
---
+++ server/index.js
@@ -0,0 +1,55 @@
+var express = require("express")
+var cors = require("cors")
+
+var RegisterModel = require("./Model/User")
+
+var app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.post('/register', (req, res) => {
+    const {name, email, password} = req.body;
+    RegisterModel.findOne({email: email})
+    .then(user => {
+        if(user) {
+            res.json("User already Registered")
+        }
+        else{
+            RegisterModel.create({name:name, email:email, password:password})
+            .then(result => {
+                res.json("User Registered succefully")
+            })
+            .catch(err => res.json(err))
+        }
+    })
+    .catch(err => {res.json(err)})
+})
+
+app.post('/login', (req, res) => {
+    const {email, password} = req.body;
+    RegisterModel.findOne({email:email})
+    .then(user => {
+        if(user){
+            RegisterModel.findOne({password:password})
+            .then(pass => {
+                if(pass){
+                    res.json("Login Successfully")
+                }
+                else{
+                    res.json("Incorrect Passowrd")
+                }
+            })
+        }
+        else{
+            res.json("Incorrect Email");
+        }
+    })
+});
+
+
+
+
+app.listen(3001, () => {
+    console.log("Server is running")
+})
