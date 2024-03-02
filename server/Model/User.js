f81d45565bd09bac951adc316412f0bdcfdd8b2f
Maaz-Hassan <sengineer804@gmail.com>
2024-03-02 10:25:27 +0500

    update

--- server/Model/User.js
+++ server/Model/User.js
@@ -1,13 +1,26 @@
-var mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
+// MongoDB Atlas connection string
+const uri = 'mongodb+srv://sengineer804:maaz1234@cluster0.197khlz.mongodb.net/test?retryWrites=true&w=majority';
 
+// Connect to MongoDB Atlas
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true // Optional, but recommended to avoid deprecation warning
+}).then(() => {
+  console.log('Connected to MongoDB Atlas');
+}).catch(err => {
+  console.error('Error connecting to MongoDB Atlas:', err);
+});
 
-var RegisterSchema = new mongoose.Schema({
-    name: String,
-    email:String,
-    password:String
-})
+// Define and export your Mongoose model
+const RegisterSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+});
 
-const RegisterModel = mongoose.model("user", RegisterSchema);
+const RegisterModel = mongoose.model('user', RegisterSchema);
 
 module.exports = RegisterModel;
